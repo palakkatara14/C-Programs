7af2ca73d1a720a9255a9d8909ce678696d1e072
palakkatara14 <122154471+palakkatara14@users.noreply.github.com>
2023-01-10 17:09:18 +0530

    Update CProgram.c

--- CProgram.c
+++ CProgram.c
@@ -1050,4 +1050,37 @@ int i=5;
        printf("%d ",arr2[i]);
 
    }
+              
+              
+              
+#include<stdio.h>             
+#include<math.h>
+int arms(int);
+
+int main()
+{
+    int r;
+    printf("enter range");
+    scanf("%d",&r);
+    for(int i=1;i<=r;i++)
+    {
+        arms(i);
+    }
+
+    return 0;
+}
+int arms(int a)
+{
+    int k=a,sum=0,d;
+    int l=log10(a)+1;
+    while(a!=0)
+    {
+        d=a%10;
+        a=a/10;
+        sum=sum +pow(d,l);
+    }
+    if(sum==k)
+    printf("%d\n",k);
+    
+}
    
